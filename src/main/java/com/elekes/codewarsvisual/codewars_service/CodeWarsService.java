b481550a89daaceee690754bda950deddcd12d05
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-11 16:36:59 +0100

    Add method to fetch all completed katas for user

--- src/main/java/com/elekes/codewarsvisual/codewars_service/CodeWarsService.java
+++ src/main/java/com/elekes/codewarsvisual/codewars_service/CodeWarsService.java
@@ -1,6 +1,9 @@
 package com.elekes.codewarsvisual.codewars_service;
 
 import com.elekes.codewarsvisual.apimodel.CodeChallenge;
+import com.elekes.codewarsvisual.apimodel.completed.CompletedChallenge;
+import com.elekes.codewarsvisual.apimodel.completed.CompletedPage;
+import com.elekes.codewarsvisual.apimodel.user.User;
 import com.elekes.codewarsvisual.entity.Kata;
 import com.elekes.codewarsvisual.repository.KataRepository;
 import com.elekes.codewarsvisual.util.ApiToDbMapper;
@@ -8,6 +11,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CodeWarsService {
@@ -19,12 +25,29 @@ public class CodeWarsService {
     private KataRepository kataRepository;
     private ApiToDbMapper mapper = new ApiToDbMapper();
 
-    public Kata getCodeChallengeFromCodeWars(String codewarsId) {
+    public void persistCodeChallengeFromCodeWars(String codewarsId) {
         String url = baseUrl + "code-challenges/" + codewarsId;
         CodeChallenge response = restTemplate.getForObject(url, CodeChallenge.class);
         Kata toSave = mapper.codeChallengeToKata.apply(response);
         kataRepository.save(toSave);
-        return toSave;
+    }
+
+    public void getAllCompletedForUser(String username, String apiKey) {
+        int page = 0;
+        List<CompletedChallenge> challenges = new ArrayList<>();
+        CompletedPage pageResponse = restTemplate.getForObject(urlForPage(page, username, apiKey), CompletedPage.class);
+        challenges.addAll(pageResponse.getData());
+        int pagesTotal = pageResponse.getTotalPages();
+        if (pagesTotal > 1) {
+            for (int i = 1; i < pagesTotal; i++) {
+                pageResponse = restTemplate.getForObject(urlForPage(i, username, apiKey), CompletedPage.class);
+                challenges.addAll(pageResponse.getData());
+            }
+        }
+    }
+
+    private String urlForPage(int p, String username, String apiKey) {
+        return baseUrl + "users/" + username + "/code-challenges/completed?page=" + p + ",access_key=" + apiKey;
     }
 
 }
