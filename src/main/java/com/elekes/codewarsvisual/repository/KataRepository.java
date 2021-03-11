ffb30f1884ea5763a9b13033c9ae828d62c02314
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-11 18:19:52 +0100

    Fetch list of katas by codewarsId in KataRepository

--- src/main/java/com/elekes/codewarsvisual/repository/KataRepository.java
+++ src/main/java/com/elekes/codewarsvisual/repository/KataRepository.java
@@ -2,6 +2,12 @@ package com.elekes.codewarsvisual.repository;
 
 import com.elekes.codewarsvisual.entity.Kata;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+import java.util.Set;
 
 public interface KataRepository extends JpaRepository<Kata, Long> {
+
+    List<Kata> findByCodewarsIdIn(Set<String> idList);
 }
