dd506140e893c73eac6c9957db0340862cb9c6a7
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-11 18:18:24 +0100

    Remove persisting Kata from CodeWarsService

--- src/main/java/com/elekes/codewarsvisual/service/CodeWarsService.java
+++ src/main/java/com/elekes/codewarsvisual/service/CodeWarsService.java
@@ -1,13 +1,11 @@
-package com.elekes.codewarsvisual.codewars_service;
+package com.elekes.codewarsvisual.service;
 
 import com.elekes.codewarsvisual.apimodel.CodeChallenge;
 import com.elekes.codewarsvisual.apimodel.completed.CompletedChallenge;
 import com.elekes.codewarsvisual.apimodel.completed.CompletedPage;
 import com.elekes.codewarsvisual.apimodel.user.User;
 import com.elekes.codewarsvisual.entity.Kata;
-import com.elekes.codewarsvisual.repository.KataRepository;
 import com.elekes.codewarsvisual.util.ApiToDbMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -21,15 +19,13 @@ public class CodeWarsService {
     @Value("${codewars.base.url}")
     private String baseUrl;
     private RestTemplate restTemplate = new RestTemplate();
-    @Autowired
-    private KataRepository kataRepository;
     private ApiToDbMapper mapper = new ApiToDbMapper();
 
-    public void persistCodeChallengeFromCodeWars(String codewarsId) {
+    public Kata getCodeChallengeFromCodeWars(String codewarsId) {
         String url = baseUrl + "code-challenges/" + codewarsId;
         CodeChallenge response = restTemplate.getForObject(url, CodeChallenge.class);
         Kata toSave = mapper.codeChallengeToKata.apply(response);
-        kataRepository.save(toSave);
+        return toSave;
     }
 
     /**
@@ -41,7 +37,7 @@ public class CodeWarsService {
         System.out.println(user);
     }
 
-    public void getAllCompletedForUser(String username, String apiKey) {
+    public List<CompletedChallenge> getAllCompletedForUser(String username, String apiKey) {
         int page = 0;
         List<CompletedChallenge> challenges = new ArrayList<>();
         CompletedPage pageResponse = restTemplate.getForObject(urlForPage(page, username, apiKey), CompletedPage.class);
@@ -53,6 +49,7 @@ public class CodeWarsService {
                 challenges.addAll(pageResponse.getData());
             }
         }
+        return challenges;
     }
 
     private String urlForPage(int p, String username, String apiKey) {
