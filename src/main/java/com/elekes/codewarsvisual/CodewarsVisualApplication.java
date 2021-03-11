9722f3d4ef4ad971f21406b618e059b498bf4425
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-11 13:29:48 +0100

    Initial commit

---
+++ src/main/java/com/elekes/codewarsvisual/CodewarsVisualApplication.java
@@ -0,0 +1,13 @@
+package com.elekes.codewarsvisual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodewarsVisualApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodewarsVisualApplication.class, args);
+    }
+
+}
