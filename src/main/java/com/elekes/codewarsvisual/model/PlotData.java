677018029de1d7f9ed5ff022924546d042baac3f
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-12 13:26:09 +0100

    Fill in PlotData with missing ranks (count 0)

--- src/main/java/com/elekes/codewarsvisual/model/PlotData.java
+++ src/main/java/com/elekes/codewarsvisual/model/PlotData.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.annotation.PostConstruct;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Data
 @NoArgsConstructor
@@ -20,9 +20,22 @@ public class PlotData {
 
     public PlotData(String language, List<DataPoint> dataPoints, List<DataPoint> percentageDataPoints, int totalCount) {
         this.language = language;
-        this.dataPoints = dataPoints;
+        this.dataPoints = handleCountZero(dataPoints);
         this.totalCount = totalCount;
         this.percentageDataPoints = calculatePercentages();
+    }
+
+    private List<DataPoint> handleCountZero(List<DataPoint> dataPoints) {
+        Set<String> necessary = Set.of("8 kyu", "7 kyu", "6 kyu", "5 kyu", "4 kyu", "3 kyu", "2 kyu", "1 kyu");
+        Set<String> existing = dataPoints.stream().map(DataPoint::getLabel).collect(Collectors.toSet());
+        List<DataPoint> missing = necessary
+                .stream()
+                .filter(n -> !existing.contains(n))
+                .map(n -> new DataPoint(0l, n))
+                .collect(Collectors.toList());
+        dataPoints.addAll(missing);
+        Collections.sort(dataPoints);
+        return dataPoints;
     }
 
     private List<DataPoint> calculatePercentages() {
