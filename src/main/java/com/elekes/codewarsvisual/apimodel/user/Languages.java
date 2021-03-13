1de903fddad32126609605fd5639c38c6326a4a7
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-13 13:31:36 +0100

    Store data for each language in type ProgrammingLanguage

--- src/main/java/com/elekes/codewarsvisual/apimodel/user/Languages.java
+++ src/main/java/com/elekes/codewarsvisual/apimodel/user/Languages.java
@@ -5,33 +5,319 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public class Languages{
 
 	@JsonProperty("coffeescript")
-	private Coffeescript coffeescript;
+	private ProgrammingLanguage coffeescript;
 
 	@JsonProperty("javascript")
-	private Javascript javascript;
+	private ProgrammingLanguage javascript;
 
 	@JsonProperty("ruby")
-	private Ruby ruby;
+	private ProgrammingLanguage ruby;
 
-	public Coffeescript getCoffeescript(){
+    @JsonProperty("python")
+    private ProgrammingLanguage python;
+
+    @JsonProperty("java")
+    private ProgrammingLanguage java;
+
+    @JsonProperty("sql")
+    private ProgrammingLanguage sql;
+
+    @JsonProperty("haskell")
+    private ProgrammingLanguage haskell;
+
+    @JsonProperty("lua")
+    private ProgrammingLanguage lua;
+
+    @JsonProperty("dart")
+    private ProgrammingLanguage dart;
+
+    @JsonProperty("csharp")
+    private ProgrammingLanguage csharp;
+
+    @JsonProperty("php")
+    private ProgrammingLanguage php;
+
+    @JsonProperty("elixir")
+    private ProgrammingLanguage elixir;
+
+    @JsonProperty("cpp")
+    private ProgrammingLanguage cpp;
+
+    @JsonProperty("c")
+    private ProgrammingLanguage c;
+
+    @JsonProperty("rust")
+    private ProgrammingLanguage rust;
+
+    @JsonProperty("crystal")
+    private ProgrammingLanguage crystal;
+
+    @JsonProperty("clojure")
+    private ProgrammingLanguage clojure;
+
+    @JsonProperty("typescript")
+    private ProgrammingLanguage typescript;
+
+    @JsonProperty("fsharp")
+    private ProgrammingLanguage fsharp;
+
+    @JsonProperty("ocaml")
+    private ProgrammingLanguage ocaml;
+
+    @JsonProperty("swift")
+    private ProgrammingLanguage swift;
+
+    @JsonProperty("go")
+    private ProgrammingLanguage go;
+
+    @JsonProperty("r")
+    private ProgrammingLanguage r;
+
+    @JsonProperty("shell")
+    private ProgrammingLanguage shell;
+
+    @JsonProperty("kotlin")
+    private ProgrammingLanguage kotlin;
+
+    @JsonProperty("powershell")
+    private ProgrammingLanguage powershell;
+
+    @JsonProperty("julia")
+    private ProgrammingLanguage julia;
+
+    @JsonProperty("scala")
+    private ProgrammingLanguage scala;
+
+    @JsonProperty("nim")
+    private ProgrammingLanguage nim;
+
+    @JsonProperty("reason")
+    private ProgrammingLanguage reason;
+
+    @JsonProperty("racket")
+    private ProgrammingLanguage racket;
+
+    @JsonProperty("haxe")
+    private ProgrammingLanguage haxe;
+
+    @JsonProperty("pascal")
+    private ProgrammingLanguage pascal;
+
+    @JsonProperty("raku")
+    private ProgrammingLanguage raku;
+
+    @JsonProperty("solidity")
+    private ProgrammingLanguage solidity;
+
+    @JsonProperty("groovy")
+    private ProgrammingLanguage groovy;
+
+    @JsonProperty("vb")
+    private ProgrammingLanguage vb;
+
+    @JsonProperty("objc")
+    private ProgrammingLanguage objc;
+
+    @JsonProperty("nasm")
+    private ProgrammingLanguage nasm;
+
+    @JsonProperty("elm")
+    private ProgrammingLanguage elm;
+
+    @JsonProperty("prolog")
+    private ProgrammingLanguage prolog;
+
+    @JsonProperty("factor")
+    private ProgrammingLanguage factor;
+
+    @JsonProperty("forth")
+    private ProgrammingLanguage forth;
+
+    @JsonProperty("fortran")
+    private ProgrammingLanguage fortran;
+
+    public ProgrammingLanguage getHaskell() {
+        return haskell;
+    }
+
+    public ProgrammingLanguage getLua() {
+        return lua;
+    }
+
+    public ProgrammingLanguage getDart() {
+        return dart;
+    }
+
+    public ProgrammingLanguage getCsharp() {
+        return csharp;
+    }
+
+    public ProgrammingLanguage getPhp() {
+        return php;
+    }
+
+    public ProgrammingLanguage getElixir() {
+        return elixir;
+    }
+
+    public ProgrammingLanguage getCpp() {
+        return cpp;
+    }
+
+    public ProgrammingLanguage getC() {
+        return c;
+    }
+
+    public ProgrammingLanguage getRust() {
+        return rust;
+    }
+
+    public ProgrammingLanguage getCrystal() {
+        return crystal;
+    }
+
+    public ProgrammingLanguage getClojure() {
+        return clojure;
+    }
+
+    public ProgrammingLanguage getTypescript() {
+        return typescript;
+    }
+
+    public ProgrammingLanguage getFsharp() {
+        return fsharp;
+    }
+
+    public ProgrammingLanguage getOcaml() {
+        return ocaml;
+    }
+
+    public ProgrammingLanguage getSwift() {
+        return swift;
+    }
+
+    public ProgrammingLanguage getGo() {
+        return go;
+    }
+
+    public ProgrammingLanguage getR() {
+        return r;
+    }
+
+    public ProgrammingLanguage getShell() {
+        return shell;
+    }
+
+    public ProgrammingLanguage getKotlin() {
+        return kotlin;
+    }
+
+    public ProgrammingLanguage getPowershell() {
+        return powershell;
+    }
+
+    public ProgrammingLanguage getJulia() {
+        return julia;
+    }
+
+    public ProgrammingLanguage getScala() {
+        return scala;
+    }
+
+    public ProgrammingLanguage getNim() {
+        return nim;
+    }
+
+    public ProgrammingLanguage getReason() {
+        return reason;
+    }
+
+    public ProgrammingLanguage getRacket() {
+        return racket;
+    }
+
+    public ProgrammingLanguage getHaxe() {
+        return haxe;
+    }
+
+    public ProgrammingLanguage getPascal() {
+        return pascal;
+    }
+
+    public ProgrammingLanguage getRaku() {
+        return raku;
+    }
+
+    public ProgrammingLanguage getSolidity() {
+        return solidity;
+    }
+
+    public ProgrammingLanguage getGroovy() {
+        return groovy;
+    }
+
+    public ProgrammingLanguage getVb() {
+        return vb;
+    }
+
+    public ProgrammingLanguage getObjc() {
+        return objc;
+    }
+
+    public ProgrammingLanguage getNasm() {
+        return nasm;
+    }
+
+    public ProgrammingLanguage getElm() {
+        return elm;
+    }
+
+    public ProgrammingLanguage getProlog() {
+        return prolog;
+    }
+
+    public ProgrammingLanguage getFactor() {
+        return factor;
+    }
+
+    public ProgrammingLanguage getForth() {
+        return forth;
+    }
+
+    public ProgrammingLanguage getFortran() {
+        return fortran;
+    }
+
+    public ProgrammingLanguage getCoffeescript(){
 		return coffeescript;
 	}
 
-	public Javascript getJavascript(){
+	public ProgrammingLanguage getJavascript(){
 		return javascript;
 	}
 
-	public Ruby getRuby(){
+	public ProgrammingLanguage getRuby(){
 		return ruby;
 	}
 
-	@Override
- 	public String toString(){
-		return 
-			"Languages{" + 
-			"coffeescript = '" + coffeescript + '\'' + 
-			",javascript = '" + javascript + '\'' + 
-			",ruby = '" + ruby + '\'' + 
-			"}";
-		}
+    public ProgrammingLanguage getPython() {
+        return python;
+    }
+
+    public ProgrammingLanguage getJava() {
+        return java;
+    }
+
+    public ProgrammingLanguage getSql() {
+        return sql;
+    }
+//	@Override
+// 	public String toString(){
+//		return
+//			"Languages{" +
+//			"coffeescript = '" + coffeescript + '\'' +
+//			",javascript = '" + javascript + '\'' +
+//			",ruby = '" + ruby + '\'' +
+//			"}";
+//		}
 }
