c121f9e0a0c7b58883e8dd1339f4a8eec919179b
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-16 09:55:42 +0100

    Store password in a bcrypt encoded form

--- src/main/java/com/elekes/codewarsvisual/util/ModelToDbMapper.java
+++ src/main/java/com/elekes/codewarsvisual/util/ModelToDbMapper.java
@@ -2,17 +2,18 @@ package com.elekes.codewarsvisual.util;
 
 import com.elekes.codewarsvisual.entity.CodewarsUser;
 import com.elekes.codewarsvisual.model.authentication.RegistrationData;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class ModelToDbMapper {
 
-    /**
-     * password to be encoded when security is added!
-     */
+    private PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
     public CodewarsUser mapRegistrationDataToUser(RegistrationData regData) {
         CodewarsUser codewarsUser = CodewarsUser.builder()
                 .username(regData.getUsername())
                 .api_key(regData.getApiKey())
-                .password(regData.getPassword())
+                .password(encoder.encode(regData.getPassword()))
                 .build();
         return codewarsUser;
     }
