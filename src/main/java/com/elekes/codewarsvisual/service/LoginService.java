73489faab73aaa4e9b76074764731205c74ea7f3
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-16 11:36:19 +0100

    Return token upon registration

--- src/main/java/com/elekes/codewarsvisual/service/LoginService.java
+++ src/main/java/com/elekes/codewarsvisual/service/LoginService.java
@@ -1,6 +1,7 @@
 package com.elekes.codewarsvisual.service;
 
 import com.elekes.codewarsvisual.model.authentication.LoginCredentials;
+import com.elekes.codewarsvisual.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,14 +14,14 @@ public class LoginService {
     @Autowired
     AuthenticationManager authenticationManager;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
     public String attemptLogin(LoginCredentials credentials) {
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
                 credentials.getUsername(), credentials.getPassword());
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
-        //jwt creation
-
-        return null;
-
+        return jwtUtil.createToken(authentication);
     }
 }
