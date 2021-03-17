65da5de8585f322ceb149089e30dc29f668b77b3
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-17 14:13:38 +0100

    Add method to retrieve Authorization header from request

--- src/main/java/com/elekes/codewarsvisual/security/JwtUtil.java
+++ src/main/java/com/elekes/codewarsvisual/security/JwtUtil.java
@@ -3,18 +3,20 @@ package com.elekes.codewarsvisual.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Service;
-import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
 
 import javax.annotation.PostConstruct;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Base64;
 import java.util.Date;
 import java.util.stream.Collectors;
 
 @Service
+@Getter
 public class JwtUtil {
 
     @Value("${jwt.validityInMs}")
@@ -28,6 +30,7 @@ public class JwtUtil {
     }
 
     public String createToken(Authentication authentication) {
+        // add apiKey as a claim?
         Claims claims = Jwts.claims().setSubject(authentication.getName());
         claims.put("roles", authentication.getAuthorities()
                 .stream()
@@ -42,4 +45,15 @@ public class JwtUtil {
                 .signWith(SignatureAlgorithm.HS256, secret)
                 .compact();
     }
+
+    public String retrieveTokenFromHeader(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        String token = "";
+        if (header != null && header.startsWith("Bearer ")) {
+            token = header.substring(7);
+        }
+        System.out.println(token);
+        return token;
+    }
+
 }
