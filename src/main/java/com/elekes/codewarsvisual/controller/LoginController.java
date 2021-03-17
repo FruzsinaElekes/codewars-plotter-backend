8288323945e1c2935e7d4c163975816d9d9a38ea
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-17 18:04:38 +0100

    Set username & apiKey cookies when user logs in

--- src/main/java/com/elekes/codewarsvisual/controller/LoginController.java
+++ src/main/java/com/elekes/codewarsvisual/controller/LoginController.java
@@ -9,15 +9,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-@CrossOrigin(value = "http://localhost:3000")
+@CrossOrigin(value = "http://localhost:3000", allowCredentials = "true")
 public class LoginController {
 
     @Autowired
     private LoginService loginService;
 
     @PostMapping("/login")
-    public Token logIn(@RequestBody LoginCredentials credentials) {
-        return loginService.attemptLogin(credentials);
+    public Token logIn(@RequestBody LoginCredentials credentials, HttpServletResponse response) {
+        Token token = loginService.attemptLogin(credentials);
+        Cookie user = new Cookie("username", credentials.getUsername());
+        Cookie key = new Cookie("apiKey", loginService.getKeyForUser(credentials.getUsername()));
+        key.setHttpOnly(true);
+        user.setHttpOnly(true);
+        response.addCookie(user);
+        response.addCookie(key);
+        return token;
     }
 }
--- src/main/java/com/elekes/codewarsvisual/service/LoginService.java
+++ src/main/java/com/elekes/codewarsvisual/service/LoginService.java
@@ -1,7 +1,9 @@
 package com.elekes.codewarsvisual.service;
 
+import com.elekes.codewarsvisual.entity.CodewarsUser;
 import com.elekes.codewarsvisual.model.authentication.LoginCredentials;
 import com.elekes.codewarsvisual.model.authentication.Token;
+import com.elekes.codewarsvisual.repository.CodewarsUserRepository;
 import com.elekes.codewarsvisual.security.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -18,6 +20,9 @@ public class LoginService {
     @Autowired
     private JwtUtil jwtUtil;
 
+    @Autowired
+    private CodewarsUserRepository codewarsUserRepository;
+
     public Token attemptLogin(LoginCredentials credentials) {
 
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
@@ -25,4 +30,10 @@ public class LoginService {
         Authentication authentication = authenticationManager.authenticate(authenticationToken);
         return new Token(jwtUtil.createToken(authentication), jwtUtil.getValidityInMs());
     }
+
+    public String getKeyForUser(String username) {
+        String key = codewarsUserRepository.findByUsername(username).map(CodewarsUser::getApi_key).orElse(null);
+        return key;
+    }
+
 }
