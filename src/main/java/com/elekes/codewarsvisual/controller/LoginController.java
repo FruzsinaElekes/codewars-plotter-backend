b26bc282f625a75cf5dc556ea569371b0d03b0f4
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-17 11:37:03 +0100

    Add @CrossOrigin to LoginController

--- src/main/java/com/elekes/codewarsvisual/controller/LoginController.java
+++ src/main/java/com/elekes/codewarsvisual/controller/LoginController.java
@@ -3,11 +3,13 @@ package com.elekes.codewarsvisual.controller;
 import com.elekes.codewarsvisual.model.authentication.LoginCredentials;
 import com.elekes.codewarsvisual.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(value = "http://localhost:3000")
 public class LoginController {
 
     @Autowired
