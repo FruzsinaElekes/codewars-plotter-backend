ac42c60397c61e5316dcb7724d8c3b99de3b49c7
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-19 18:08:53 +0100

    Add endpoint to fetch all KataCompletionDetails

--- src/main/java/com/elekes/codewarsvisual/controller/UserDataController.java
+++ src/main/java/com/elekes/codewarsvisual/controller/UserDataController.java
@@ -1,11 +1,15 @@
 package com.elekes.codewarsvisual.controller;
 
 import com.elekes.codewarsvisual.model.plot.PlotData;
+import com.elekes.codewarsvisual.model.user.KataCompletionDetail;
 import com.elekes.codewarsvisual.model.user.UserSummary;
 import com.elekes.codewarsvisual.service.CodeWarsService;
+import com.elekes.codewarsvisual.service.DataRetrievalService;
 import com.elekes.codewarsvisual.service.PlotService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin(value = "http://localhost:3000")
@@ -18,15 +22,22 @@ public class UserDataController {
     @Autowired
     private PlotService plotService;
 
+    @Autowired
+    private DataRetrievalService dataRetrievalService;
+
     @GetMapping("/{username}")
     public UserSummary getUserData(@PathVariable String username) {
-
-        System.out.println();
         return codeWarsService.getUserFromCodeWars(username);
     }
 
     @GetMapping("/{username}/plot/{language}")
     public PlotData getPlotDataForLanguage(@PathVariable String language, @PathVariable String username) {
         return plotService.getPlotDataForLanguage(language, username);
+    }
+
+    @GetMapping("/{username}/katas")
+    public List<KataCompletionDetail> getKatas(@PathVariable String username) {
+        List<KataCompletionDetail> res = dataRetrievalService.getKataCompletionListForUser(username);
+        return res;
     }
 }
