98a36a9cdd1bbd1b92619fc4ff39caa1a67daf36
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-19 18:11:12 +0100

    Map CompletedChallenge to KataCompletionDetail

--- src/main/java/com/elekes/codewarsvisual/service/DataRetrievalService.java
+++ src/main/java/com/elekes/codewarsvisual/service/DataRetrievalService.java
@@ -2,9 +2,11 @@ package com.elekes.codewarsvisual.service;
 
 import com.elekes.codewarsvisual.apimodel.completed.CompletedChallenge;
 import com.elekes.codewarsvisual.entity.Kata;
+import com.elekes.codewarsvisual.model.user.KataCompletionDetail;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -19,17 +21,8 @@ public class DataRetrievalService {
     private KataService kataService;
 
 
-    public List<Kata> getCompletedKatasPerLanguage(String language, String username, String apiKey) {
-        List<CompletedChallenge> challenges = codeWarsService.getAllCompletedForUser(username, apiKey);
-        return persistNewAndReturnAllCompletedKatas(challenges, language);
-    }
-
     public List<Kata> getCompletedKatasPerLanguage(String language, String username) {
         List<CompletedChallenge> challenges = codeWarsService.getAllCompletedForUser(username);
-        return persistNewAndReturnAllCompletedKatas(challenges, language);
-    }
-
-    private List<Kata> persistNewAndReturnAllCompletedKatas(List<CompletedChallenge> challenges, String language) {
         Set<String> idSetForLanguage = getIdSetForLanguage(challenges, language);
         List<Kata> katasFromDb = kataService.getKatasIfCodewarsIdInSet(idSetForLanguage);
         Set<String> dbIds = katasFromDb
@@ -45,11 +38,42 @@ public class DataRetrievalService {
         return katasFromDb;
     }
 
+
     private Set<String> getIdSetForLanguage(List<CompletedChallenge> challenges, String language) {
         return challenges
                 .stream()
                 .filter(c -> c.getCompletedLanguages().contains(language))
                 .map(CompletedChallenge::getId)
                 .collect(Collectors.toSet());
+    }
+
+
+    public List<KataCompletionDetail> getKataCompletionListForUser(String username) {
+        List<CompletedChallenge> completed = codeWarsService.getAllCompletedForUser(username);
+        Set<String> completedIds = completed.stream().map(CompletedChallenge::getId).collect(Collectors.toSet());
+        List<Kata> kataList = kataService.getKatasIfCodewarsIdInSet(completedIds);
+        List<KataCompletionDetail> kataCompletionDetailList = new ArrayList<>();
+        for (Kata kata : kataList) {
+            CompletedChallenge completedChallenge = completed
+                    .stream()
+                    .filter(c -> c.getId().equals(kata.getCodewarsId()))
+                    .findFirst().orElse(null);
+            KataCompletionDetail kataCompletionDetail = KataCompletionDetail.builder()
+                    .completedAt(completedChallenge.getCompletedAt())
+                    .completedLanguages(completedChallenge.getCompletedLanguages())
+                    .category(kata.getCategory())
+                    .codewarsId(kata.getCodewarsId())
+                    .createdBy(kata.getCreatedBy())
+                    .description(kata.getDescription())
+                    .languages(kata.getLanguages())
+                    .tags(kata.getTags())
+                    .rank(kata.getRank())
+                    .url(kata.getUrl())
+                    .name(kata.getName())
+                    .slug(kata.getSlug())
+                    .build();
+            kataCompletionDetailList.add(kataCompletionDetail);
+        }
+        return kataCompletionDetailList;
     }
 }
