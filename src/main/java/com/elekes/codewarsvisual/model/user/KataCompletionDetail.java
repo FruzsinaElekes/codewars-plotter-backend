cd4bff40a2b0a55ca7d75dfe11dc321a7c48ec19
FruzsinaElekes <elekes.fr@gmail.com>
2021-03-19 18:10:27 +0100

    Add class KataCompletionDetail

---
+++ src/main/java/com/elekes/codewarsvisual/model/user/KataCompletionDetail.java
@@ -0,0 +1,29 @@
+package com.elekes.codewarsvisual.model.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.LinkedList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class KataCompletionDetail {
+
+    private String codewarsId;
+    private String name;
+    private String slug;
+    private String category;
+    private List<String> languages = new LinkedList<>();
+    private String rank;
+    private String url;
+    private String description;
+    private String createdBy;
+    private List<String> tags = new LinkedList<>();
+    private List<String> completedLanguages;
+    private String completedAt;
+}
